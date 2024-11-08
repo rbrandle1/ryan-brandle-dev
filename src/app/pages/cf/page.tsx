4ebc95d756014cc9ba7dc7262376d4ca375faeb8
Ryan Brandle <r.brandle@gmail.com>
2024-11-08 15:03:33 -0700

    added new page directories

---
+++ src/app/pages/cf/page.tsx
@@ -0,0 +1,7 @@
+// 'use client';
+
+const CfPage = () => {
+	return <div>CF Project Page</div>;
+};
+
+export default CfPage;
---
+++ src/app/pages/insp/page.tsx
@@ -0,0 +1,7 @@
+// 'use client';
+
+const InspPage = () => {
+	return <div>Inspirato Project Page</div>;
+};
+
+export default InspPage;
