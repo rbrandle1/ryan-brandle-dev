858665cbd4a9e7ba5ffbc14ccdd74dcbf187f533
Ryan Brandle <r.brandle@gmail.com>
2024-12-13 17:37:13 -0700

    added more type styles and added to style page

---
+++ src/app/(pages)/style/page.tsx
@@ -0,0 +1,68 @@
+// 'use client';
+import styles from './style.module.scss';
+
+const StylePage = () => {
+	return (
+		<div className={styles.home}>
+			<main className={styles.main}>
+				<section className={styles.section}>
+					<div className={styles.container}>
+						<div className={styles.displayLg}>Display LG</div>
+						<div className={styles.displaySm}>Display SM</div>
+						<div className={styles.h1}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h2}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h3}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h4}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h5}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h6}>How vexingly quick daft zebras jump</div>
+						<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
+						<div className={styles.callout}>How vexingly quick daft zebras jump</div>
+						<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
+						<div className={styles.pLg}>How vexingly quick daft zebras jump</div>
+						<div className={styles.p}>How vexingly quick daft zebras jump</div>
+						<div className={styles.pSm}>How vexingly quick daft zebras jump</div>
+					</div>
+				</section>
+				<section data-section='dark' className={styles.section}>
+					<div className={styles.container}>
+						<div className={styles.displayLg}>Display LG</div>
+						<div className={styles.displaySm}>Display SM</div>
+						<div className={styles.h1}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h2}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h3}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h4}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h5}>How vexingly quick daft zebras jump</div>
+						<div className={styles.h6}>How vexingly quick daft zebras jump</div>
+						<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
+						<div className={styles.callout}>How vexingly quick daft zebras jump</div>
+						<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
+						<div className={styles.pLg}>How vexingly quick daft zebras jump</div>
+						<div className={styles.p}>How vexingly quick daft zebras jump</div>
+						<div className={styles.pSm}>How vexingly quick daft zebras jump</div>
+					</div>
+				</section>
+				<section className={styles.section}>
+					<div className={styles.container}>
+						<div className={styles.space3xs}></div>
+						<div className={styles.space2xs}></div>
+						<div className={styles.spaceXs}></div>
+						<div className={styles.spaceSm}></div>
+						<div className={styles.spaceBase}></div>
+						<div className={styles.spaceMd}></div>
+						<div className={styles.spaceLg}></div>
+						<div className={styles.spaceXl}></div>
+						<div className={styles.space2xl}></div>
+						<div className={styles.space3xl}></div>
+						<div className={styles.space4xl}></div>
+						<div className={styles.space5xl}></div>
+						<div className={styles.space6xl}></div>
+						<div className={styles.space7xl}></div>
+						<div className={styles.space8xl}></div>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
+};
+
+export default StylePage;
