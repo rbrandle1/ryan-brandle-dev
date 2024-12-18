775014f873efba949785d558c57cfe6a334a2014
Ryan Brandle <r.brandle@gmail.com>
2024-12-18 16:15:38 -0700

    refactored accent edge to be a global mixin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import styles from './Footer.module.scss';
+import Image from 'next/image';
 
 interface FooterProps {
 	exampleA?: string;
@@ -6,7 +7,12 @@ interface FooterProps {
 }
 
 const Footer = ({ exampleA, exampleB }: FooterProps) => {
-	return <div className={styles.footer}>footer here</div>;
+	return (
+		<div className={styles.footer}>
+			<Image className={styles.logo} src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
+			<div>Footer stuff here</div>
+		</div>
+	);
 };
 
 export default Footer;
