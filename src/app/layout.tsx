925f46c55871aad117ef1e3f5d456f2e6a4c55a8
Ryan Brandle <r.brandle@gmail.com>
2024-12-18 15:32:58 -0700

    removed FOUC by refactoring how fonts are loaded in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,32 @@
 import type { Metadata } from 'next';
+import { Oswald, Source_Sans_3, Source_Serif_4 } from 'next/font/google';
 import '@/styles/base.scss';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+
+// Importing the fonts
+const oswald = Oswald({
+	weight: '700',
+	subsets: ['latin'],
+});
+
+const sourceSans = Source_Sans_3({
+	weight: ['400', '700', '900'],
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+});
+
+const sourceSerif = Source_Serif_4({
+	weight: ['400', '700', '900'],
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+});
+
+// Function to use the font constants, which satisfies typescript requirements.
+// This function does nothing but ensures the fonts are referenced
+const useFonts = () => {
+	return { oswald, sourceSans, sourceSerif };
+};
 
 export const metadata: Metadata = {
 	title: 'Ryan Brandle',
@@ -13,6 +38,8 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	useFonts();
+
 	return (
 		<html lang='en'>
 			<body>
