a31538ffcd1c1ebffb3b554e348444217c60a313
Ryan Brandle <r.brandle@gmail.com>
2024-12-18 17:52:57 -0700

    refactored nested properties for new dart sass warnings, solved sass warning by changing length to list.length in has-hover mixin

--- src/app/(pages)/style/page.tsx
+++ src/app/(pages)/style/page.tsx
@@ -2,83 +2,132 @@
 import cn from 'classnames';
 import styles from './style.module.scss';
 
+/* Todo: 
+	* Continue typography classes/mixins. links, link-with-icon
+	* Make buttons/link
+	* Make theme picker component
+	* Prep all necessary svgs, figure out if need to recreate logo.
+	* Finish header
+	* Finish footer
+	* Begin stubbing general layout and components within.
+
+
+	* FUTURE EXPLORATION:
+	* Set typography utils and classes... explore more modern ways to do this? has:, etc?
+  * Add shadows/elevation, gradients, animation timing and others that don't exist in figma
+  * Try setting base space from 1.6 to 1.8. Also, might need to adjust the real big spaces % max to be larger for a more noticeable difference. Can also run the calculation to a raw value to enhance performance.
+*/
+
+const TypographyExamples = () => {
+	return (
+		<>
+			<div className={styles.displayLg}>
+				Display LG <span className={styles.altFont}>&amp;</span>
+			</div>
+			<div className={styles.displaySm}>
+				Display SM <span className={styles.altFont}>&amp;</span>
+			</div>
+			<div className={styles.h1}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.h2}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.h3}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.h4}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.h5}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.h6}>
+				How vexingly <span className={styles.altFont}>&amp;</span> quick daft zebras jump
+			</div>
+			<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
+			<div className={styles.callout}>How vexingly quick daft zebras jump</div>
+			<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
+			<div className={styles.pLg}>
+				In the vast cosmic ocean, where celestial bodies dance to the rhythm of gravity, one might ponder the profound
+				interconnectedness of all things.{' '}
+				<a href='#' className={styles.link}>
+					Lorem ipsum dolor
+				</a>{' '}
+				sit amet, consectetur adipiscing elit, where quasars beam their enigmatic light across spacetime, and galaxies
+				weave tapestries of stardust. Nullam sapien sem, tincidunt eget quam nec, ultricies fringilla eros. Integer sit
+				amet nunc vel lacus fermentum malesuada. Suspendisse potenti, for the universe is not obligated to make sense to
+				us, yet we persist in decoding its mysteries. Sed nec massa nec eros varius dictum et ut arcu, an echo of
+				humanity’s unyielding curiosity.
+			</div>
+			<div className={styles.p}>
+				In the vast cosmic ocean, where celestial bodies dance to the rhythm of gravity, one might ponder the profound
+				interconnectedness of all things.{' '}
+				<a href='#' className={styles.link}>
+					Lorem ipsum dolor
+				</a>{' '}
+				sit amet, consectetur adipiscing elit, where quasars beam their enigmatic light across spacetime, and galaxies
+				weave tapestries of stardust. Nullam sapien sem, tincidunt eget quam nec, ultricies fringilla eros. Integer sit
+				amet nunc vel lacus fermentum malesuada. Suspendisse potenti, for the universe is not obligated to make sense to
+				us, yet we persist in decoding its mysteries. Sed nec massa nec eros varius dictum et ut arcu, an echo of
+				humanity’s unyielding curiosity.
+			</div>
+			<div className={styles.pSm}>
+				In the vast cosmic ocean, where celestial bodies dance to the rhythm of gravity, one might ponder the profound
+				interconnectedness of all things.{' '}
+				<a href='#' className={styles.link}>
+					Lorem ipsum dolor
+				</a>{' '}
+				sit amet, consectetur adipiscing elit, where quasars beam their enigmatic light across spacetime, and galaxies
+				weave tapestries of stardust. Nullam sapien sem, tincidunt eget quam nec, ultricies fringilla eros. Integer sit
+				amet nunc vel lacus fermentum malesuada. Suspendisse potenti, for the universe is not obligated to make sense to
+				us, yet we persist in decoding its mysteries. Sed nec massa nec eros varius dictum et ut arcu, an echo of
+				humanity’s unyielding curiosity.
+			</div>
+			<div className={styles.caption}>How vexingly quick daft zebras jump</div>
+		</>
+	);
+};
+
+const SpacingExamples = () => {
+	return (
+		<>
+			<div className={styles.space3xs}></div>
+			<div className={styles.space2xs}></div>
+			<div className={styles.spaceXs}></div>
+			<div className={styles.spaceSm}></div>
+			<div className={styles.spaceBase}></div>
+			<div className={styles.spaceMd}></div>
+			<div className={styles.spaceLg}></div>
+			<div className={styles.spaceXl}></div>
+			<div className={styles.space2xl}></div>
+			<div className={styles.space3xl}></div>
+			<div className={styles.space4xl}></div>
+			<div className={styles.space5xl}></div>
+			<div className={styles.space6xl}></div>
+			<div className={styles.space7xl}></div>
+			<div className={styles.space8xl}></div>
+		</>
+	);
+};
+
 const StylePage = () => {
 	return (
 		<div className={styles.home}>
 			<main className={styles.main}>
-				<div className={styles.accentEdge}>
-					<section className={styles.section}>
-						<div className={styles.container}>
-							<div className={styles.displayLg}>Display LG</div>
-							<div className={styles.displaySm}>Display SM</div>
-							<div className={styles.h1}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h2}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h3}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h4}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h5}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h6}>How vexingly quick daft zebras jump</div>
-							<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
-							<div className={styles.callout}>How vexingly quick daft zebras jump</div>
-							<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
-							<div className={styles.pLg}>How vexingly quick daft zebras jump</div>
-							<div className={styles.p}>How vexingly quick daft zebras jump</div>
-							<div className={styles.pSm}>How vexingly quick daft zebras jump</div>
-						</div>
-					</section>
-					<section data-section='dark' className={styles.section}>
-						<div className={styles.container}>
-							<div className={styles.displayLg}>Display LG</div>
-							<div className={styles.displaySm}>Display SM</div>
-							<div className={styles.h1}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h2}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h3}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h4}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h5}>How vexingly quick daft zebras jump</div>
-							<div className={styles.h6}>How vexingly quick daft zebras jump</div>
-							<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
-							<div className={styles.callout}>How vexingly quick daft zebras jump</div>
-							<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
-							<div className={styles.pLg}>How vexingly quick daft zebras jump</div>
-							<div className={styles.p}>How vexingly quick daft zebras jump</div>
-							<div className={styles.pSm}>How vexingly quick daft zebras jump</div>
-						</div>
-					</section>
-				</div>
-				<section className={styles.section}>
+				<section className={cn(styles.section, styles.accentEdge)}>
 					<div className={styles.container}>
-						<div className={styles.space3xs}></div>
-						<div className={styles.space2xs}></div>
-						<div className={styles.spaceXs}></div>
-						<div className={styles.spaceSm}></div>
-						<div className={styles.spaceBase}></div>
-						<div className={styles.spaceMd}></div>
-						<div className={styles.spaceLg}></div>
-						<div className={styles.spaceXl}></div>
-						<div className={styles.space2xl}></div>
-						<div className={styles.space3xl}></div>
-						<div className={styles.space4xl}></div>
-						<div className={styles.space5xl}></div>
-						<div className={styles.space6xl}></div>
-						<div className={styles.space7xl}></div>
-						<div className={styles.space8xl}></div>
+						<TypographyExamples />
+					</div>
+				</section>
+				<section data-section='dark' className={styles.section}>
+					<div className={styles.container}>
+						<TypographyExamples />
 					</div>
 				</section>
 				<section className={cn(styles.section, styles.accentEdge)}>
 					<div className={styles.container}>
-						<div className={styles.displayLg}>Display LG</div>
-						<div className={styles.displaySm}>Display SM</div>
-						<div className={styles.h1}>How vexingly quick daft zebras jump</div>
-						<div className={styles.h2}>How vexingly quick daft zebras jump</div>
-						<div className={styles.h3}>How vexingly quick daft zebras jump</div>
-						<div className={styles.h4}>How vexingly quick daft zebras jump</div>
-						<div className={styles.h5}>How vexingly quick daft zebras jump</div>
-						<div className={styles.h6}>How vexingly quick daft zebras jump</div>
-						<div className={styles.calloutLg}>How vexingly quick daft zebras jump</div>
-						<div className={styles.callout}>How vexingly quick daft zebras jump</div>
-						<div className={styles.calloutSm}>How vexingly quick daft zebras jump</div>
-						<div className={styles.pLg}>How vexingly quick daft zebras jump</div>
-						<div className={styles.p}>How vexingly quick daft zebras jump</div>
-						<div className={styles.pSm}>How vexingly quick daft zebras jump</div>
+						<SpacingExamples />
 					</div>
 				</section>
 			</main>
