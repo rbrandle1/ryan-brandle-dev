51ee246cd07b72442f2d86ae80b3053b35c866f0
Ryan Brandle <r.brandle@gmail.com>
2025-01-08 16:50:37 -0700

    experimenting with logo animations

--- src/components/LogoSwitcher/LogoSwitcher.tsx
+++ src/components/LogoSwitcher/LogoSwitcher.tsx
@@ -1,23 +1,38 @@
-import { FC } from 'react';
+import cn from 'classnames';
+import { FC, useEffect, useState } from 'react';
 import IconLogoM from '../Icons/IconLogoM';
 import IconLogoC from '../Icons/IconLogoC';
 import IconLogoY from '../Icons/IconLogoY';
 import IconLogoP from '../Icons/IconLogoP';
 import { Theme } from '@/types/themeTypes';
+import styles from './LogoSwitcher.module.scss';
 
 interface LogoSwitcherProps {
 	theme: Theme;
 }
 
 const LogoSwitcher: FC<LogoSwitcherProps> = ({ theme = 'm' }) => {
+	const [currentTheme, setCurrentTheme] = useState(theme);
+	const [isVisible, setIsVisible] = useState(true);
+
+	useEffect(() => {
+		setIsVisible(false);
+		const timer = setTimeout(() => {
+			setCurrentTheme(theme);
+			setIsVisible(true);
+		}, 500);
+
+		return () => clearTimeout(timer);
+	}, [theme]);
+
 	const logos = {
-		m: <IconLogoM aria-hidden />,
-		c: <IconLogoC aria-hidden />,
-		p: <IconLogoP aria-hidden />,
-		y: <IconLogoY aria-hidden />,
+		m: <IconLogoM className={cn(styles.logo, isVisible && styles.fadeIn)} aria-hidden />,
+		c: <IconLogoC className={cn(styles.logo, isVisible && styles.fadeIn)} aria-hidden />,
+		p: <IconLogoP className={cn(styles.logo, isVisible && styles.fadeIn)} aria-hidden />,
+		y: <IconLogoY className={cn(styles.logo, isVisible && styles.fadeIn)} aria-hidden />,
 	};
 
-	return logos[theme];
+	return <div className={styles.logoContainer}>{logos[currentTheme]}</div>;
 };
 
 export default LogoSwitcher;
