c5ba1b54b5e16698548991a6627df92bed2d8c22
Ryan Brandle <r.brandle@gmail.com>
2025-01-07 17:34:31 -0700

    added active link logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,42 @@
+'use client';
+import React, { useState } from 'react';
 import cn from 'classnames';
-import styles from './Header.module.scss';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
 import ThemePicker from '../ThemePicker/ThemePicker';
+import styles from './Header.module.scss';
 
-interface HeaderProps {
-	isMenuOpen?: boolean;
-	setIsMenuOpen?: (isOpen: boolean) => void;
-}
+const PAGES = [
+	{ url: '/', label: 'Work' },
+	{ url: '/style', label: 'About' },
+] as const;
 
-//add isActive booleans to links
+const Header = () => {
+	const pathname = usePathname();
+	const [activeLink, setActiveLink] = useState(pathname);
 
-const Header = ({ isMenuOpen, setIsMenuOpen }: HeaderProps) => {
+	const handleLinkClick = (url: string) => {
+		setActiveLink(url);
+	};
+
 	return (
 		<header className={styles.header}>
 			<Image className={styles.logo} src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
 			<nav className={styles.navContainer}>
 				<ul className={styles.nav}>
-					<li>
-						<a href='/' className={cn(styles.link)} data-text='Work'>
-							Work
-						</a>
-					</li>
-					<li>
-						<a href='/style' className={styles.link} data-text='About'>
-							About
-						</a>
-					</li>
+					{PAGES.map(({ url, label }, i) => (
+						<li key={i}>
+							<Link
+								href={url}
+								className={cn(styles.link, activeLink === url && styles.isActive)}
+								data-text={label}
+								onClick={() => handleLinkClick(url)}
+							>
+								{label}
+							</Link>
+						</li>
+					))}
 				</ul>
 				<ThemePicker />
 			</nav>
