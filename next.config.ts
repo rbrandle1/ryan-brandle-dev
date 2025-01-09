5d7a86143a8cc40d47683bb6d70623903afc4029
Ryan Brandle <r.brandle@gmail.com>
2025-01-09 16:12:16 -0700

    updated next.config to force animations to load by default to get around local component modules not loading animation keyframes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	sassOptions: {
-		includePaths: ['./src/styles'],
+		includePaths: ['./src/styles/**/*', './src/components/**/*', './src/app/**/*'],
+		prependData: `@use "@/styles/utils/_animation.scss" as *;`,
 	},
 };
 
