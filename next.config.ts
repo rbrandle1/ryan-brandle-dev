6ba47739132c7622aae70c8f8387894efdd7c651
Ryan Brandle <r.brandle@gmail.com>
2025-01-10 10:56:14 -0700

    refactored  next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	sassOptions: {
-		includePaths: ['./src/styles'],
+		includePaths: [
+			path.join(__dirname, 'src/components'), // For .src/components and deeper levels
+			path.join(__dirname, 'src/app'), // For .src/app and its subdirectories
+		],
+		prependData: `@use "@/styles/utils/animation.scss" as *;`,
 	},
 };
 
