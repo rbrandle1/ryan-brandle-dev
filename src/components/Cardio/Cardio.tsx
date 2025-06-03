3e95ae7e9ff5576591e87f250b480fda92eadc61
Ryan Brandle <r.brandle@gmail.com>
2025-06-03 15:24:46 -0600

    text changes

--- src/components/Cardio/Cardio.tsx
+++ src/components/Cardio/Cardio.tsx
@@ -1,6 +1,5 @@
 // Practice from Tim:
 // Swagger docs:  https://vlaurencena.github.io/harry-potter-openapi-swagger-ui/#/Books/get_books
-
 // end point for books: https://potterapi-fedeperin.vercel.app/en/books
 
 // 1. Create a parent container that holds state
@@ -9,109 +8,19 @@
 // 4. pass data to child
 // 5. display data on child.
 
-// 'use client';
-// import { useEffect, useState } from 'react';
-
-// const MY_DATA = 'https://jsonplaceholder.typicode.com/todos/';
-
-// const Cardio = () => {
-// 	const [data, setData] = useState(null);
-
-// 	useEffect(() => {
-// 		try {
-// 			const fetchData = async () => {
-// 				const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
-// 				const data = await res.json();
-// 				setData(data);
-// 			};
-// 			fetchData();
-// 		} catch (error) {
-// 			console.log('Error fetching data', error);
-// 		}
-// 	}, []);
-
-// 	return data ? <div>There is data!</div> : null;
-// };
-
-// export default Cardio;
-
-// 'use-client';
-// import { useState, useEffect } from 'react';
-
-// const MY_DATA = 'api';
-
-// const Cardio = () => {
-// 	const [data, setData] = useState(null);
-
-// 	useEffect(() => {
-// 		const fetchData = async () => {
-// 			try {
-// 				const res = await fetch(MY_DATA);
-// 				const data = await res.json();
-// 				setData(data);
-// 			} catch (error) {
-// 				console.log('there has been an error fetching data', error);
-// 			}
-// 		};
-
-// 		fetchData();
-// 	}, []);
-
-// 	return data ? <div>there is data</div> : null;
-// };
-
-// 'use-client';
-
-// import { useState, useEffect } from 'react';
-// const MY_DATA = 'https://jsonplaceholder.typicode.com/todos/';
-
-// interface CardioProps {
-// 	title: string;
-// 	description: string;
-// 	isAvailable: boolean;
-// }
-
-// type Data = CardioProps[];
-
-// const Cardio = () => {
-// 	const [data, setData] = useState<Data | null>(null);
-
-// 	useEffect(() => {
-// 		const fetchData = async () => {
-// 			try {
-// 				const res = await fetch(MY_DATA);
-// 				const data = await res.json();
-// 				setData(data);
-// 			} catch (error) {
-// 				console.log('there is an error', error);
-// 			}
-// 		};
-// 		fetchData();
-// 	}, []);
-
-// return data ? (
-// 	<>
-// 		<div>There is data and it is {data.length}</div>
-// 		{data.map((d, i) => (
-// 			<div key={i}>{d.title}</div>
-// 		))}
-// 	</>
-// ) : null;
-// };
-
 'use client';
 import { useEffect, useState } from 'react';
 
 const MY_DATA = 'https://potterapi-fedeperin.vercel.app/en/books';
 
-interface Todo {
+interface Books {
 	title: string;
 	description: string;
 }
 
-type Data = Todo[];
+type Data = Books[];
 
-const Todo = () => {
+const Cardio = () => {
 	const [data, setData] = useState<Data | null>(null);
 
 	useEffect(() => {
@@ -137,4 +46,4 @@ const Todo = () => {
 	) : null;
 };
 
-export default Todo;
+export default Cardio;
